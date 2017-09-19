819a97780ffb662a9610a6cd4e67cc2e3af491ff
thismax <max.g.jacobs@gmail.com>
2017-09-19 09:47:54 -0700

    opting to use request-promise instead of request

--- server/api/spotify.js
+++ server/api/spotify.js
@@ -2,3 +2,14 @@ const request = require('request');
 const querystring = require('querystring');
 const clientID = require('./config').clientID;
 const clientSecret = require('./config').clientSecret;
+
+cont authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
