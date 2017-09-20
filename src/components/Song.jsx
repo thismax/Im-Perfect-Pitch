5e55872895d24d6f2aaaa8cbd5b6a2743d62282d
thismax <max.g.jacobs@gmail.com>
2017-09-19 17:50:48 -0700

    styling

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -6,7 +6,7 @@ const Song = ({song, remove, selectSong}) => {
       <span className='track' onClick={()=>{selectSong(song.uri)}}>Track Name: {song.name}  </span><br/>
       <span className='artist' onClick={()=>{selectSong(song.uri)}}>Artist: {song.artist}  </span><br/>
       <span className='album' onClick={()=>{selectSong(song.uri)}}>Album: {song.album}  </span><br/>
-      <span className='delete' onClick={()=>{remove(song.id)}}>(click to delete)</span>
+      <i className="fa fa-times-circle-o fa-2x" aria-hidden="true" onClick={()=>{remove(song.id)}}></i>
     </div>
   )
 }
