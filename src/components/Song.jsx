0dceded1ae700ed306185063554065167ee5e5c2
thismax <max.g.jacobs@gmail.com>
2017-09-19 21:44:29 -0700

    style tweeks

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Song = ({song, remove, selectSong}) => {
   return (
     <div className="song">
-      <span className='track' onClick={()=>{selectSong(song.uri)}}>Track Name: {song.name}  </span><br/>
-      <span className='artist' onClick={()=>{selectSong(song.uri)}}>Artist: {song.artist}  </span><br/>
-      <span className='album' onClick={()=>{selectSong(song.uri)}}>Album: {song.album}  </span><br/>
+      <span className='track'>Track Name: {song.name}  </span><br/>
+      <span className='artist'>Artist: {song.artist}  </span><br/>
+      <span className='album'>Album: {song.album}  </span><br/>
+      <i className="fa fa-play-circle-o fa-2x" aria-hidden="true" onClick={()=>{selectSong(song.uri)}}></i>
       <i className="fa fa-times-circle-o fa-2x" aria-hidden="true" onClick={()=>{remove(song.id)}}></i>
     </div>
   )
